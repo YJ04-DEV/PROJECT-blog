ccb3eb584fbcb5a1a181706229c10acdf655108f
나용진 <yjna@neobay.co.kr>
2023-02-02 13:53:09 +0900

    Initial commit

---
+++ src/main/java/org/dev/projectblog/ProjectBlogApplication.java
@@ -0,0 +1,13 @@
+package org.dev.projectblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectBlogApplication.class, args);
+    }
+
+}
